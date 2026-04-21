86edf0dac63b933de001fb8449e4d4916303f1fc
Eagle <dingeagle@gmail.com>
2026-04-21 14:16:15 -0400

    Filter hidden files, label dirs and files by type

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <dirent.h>
 
-int main(void) {
-	printf("corg: file organizer\n");
-	printf("Usage: corg <directory>\n");
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("corg: file organizer\n");
+		printf("Usage: corg <directory>\n");
+		return 1;
+	}
+
+	DIR *dir = opendir(argv[1]);
+	if (dir == NULL) {
+		fprintf(stderr, "corg: cannot open directory '%s'\n", argv[1]);
+		return 1;
+	}
+
+	struct dirent *entry;
+	while ((entry = readdir(dir)) != NULL) {
+		// Skip hidden files
+		if (entry->d_name[0] == '.') {
+			continue;
+		}
+
+		if (entry->d_type == DT_DIR) {
+    			printf("[DIR]  %s\n", entry->d_name);
+		} else if (entry->d_type == DT_REG) {
+			printf("[FILE] %s\n", entry->d_name);
+		} else {
+		   	printf("[OTHER] %s\n", entry->d_name);
+		}
+	}
+
+	closedir(dir);
 	return 0;
 }
