bd1c0d848484e35054bb15d8c9a3782f447887f4
Eagle <dingeagle@gmail.com>
2026-04-22 12:53:13 -0400

    Added file extension detection with get extension

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <assert.h>
+
+// get_extension(filename) returns the address of the last occurence of charater '.'
+// requires: filename not be NULL
+// time: O(n), where n is the length of filename
+const char *get_extension(const char *filename) {
+	assert(filename);
+
+	const char *tmp = filename;
+	while (*filename != '\0') {
+		if (*filename == '.') {
+			tmp = filename;
+		}
+		filename++;
+	}
+	if (*tmp != '.') return NULL;
+	return tmp;
+}
+
+
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -24,7 +45,9 @@ int main(int argc, char *argv[]) {
 		if (entry->d_type == DT_DIR) {
     			printf("[DIR]  %s\n", entry->d_name);
 		} else if (entry->d_type == DT_REG) {
-			printf("[FILE] %s\n", entry->d_name);
+			const char *ext = get_extension(entry->d_name);
+			if (ext == NULL) ext = "no ext";
+			printf("[%s] %s\n", ext, entry->d_name);
 		} else {
 		   	printf("[OTHER] %s\n", entry->d_name);
 		}
